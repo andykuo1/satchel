38802a324eeec8d1c77a5f10183eb7178d865634
andykuo1 <andykuo1supergreen@gmail.com>
2023-01-15 09:14:00 -0800

    Fix view sorting on create

--- components/App.jsx
+++ components/App.jsx
@@ -3,7 +3,7 @@ import { uuid } from '../lib/util/uuid';
 import Cursor from './cursor/Cursor';
 import { createItem } from '../stores/inv/Item';
 import { StoreProvider, useStore, ViewStore } from '../stores';
-import { addItemToInv, getInv, getView } from '../stores/transfer/InvTransfer';
+import { addItemToInv, getView } from '../stores/transfer/InvTransfer';
 import Workspace from './playground/Workspace';
 import { useViewOrganizer } from './ViewOrganizer';
 import { getCursor, hasHeldItem, setHeldItem } from '../stores/transfer/CursorTransfer';
@@ -13,8 +13,8 @@ import { createConnectorBoxInStore } from './containers/ConnectorBox';
 import { createFoundryBoxInStore } from './containers/FoundryBox';
 import { createListBoxInStore } from './containers/ListBox';
 import { createSocketBoxInStore } from './containers/SocketBox';
-import Settings from './Settings';
 import { createCraftingBoxInStore } from './containers/CraftingBox';
+import Settings from './Settings';
 
 export default function App() {
   return (
@@ -29,30 +29,27 @@ function Content() {
   const store = useStore();
 
   useYDoc();
+  useViewOrganizer();
 
   useEffect(() => {
     if (ViewStore.count(store) > 1) {
       return;
     }
-    let viewId;
     {
       let w = 6;
       let h = 4;
-      viewId = createInvBoxInStore(store, w, h, rand(), rand());
+      createInvBoxInStore(store, w, h, rand(), rand());
     }
     {
+      let viewId = createSocketBoxInStore(store, rand(), rand());
       let view = getView(store, viewId);
       let invId = view.invId;
-      let inv = getInv(store, invId);
-      inv.displayName = 'Funny';
       let item = createItem(uuid());
       item.imgSrc = '/images/potion.png';
       item.background = '#FF0000';
       item.displayName = 'Potion';
-      item.stackSize = 10;
-      let coordX = Math.floor(Math.random() * inv.width - 1);
-      let coordY = Math.floor(Math.random() * inv.height - 1);
-      addItemToInv(store, invId, item, coordX, coordY);
+      item.stackSize = 99;
+      addItemToInv(store, invId, item, 0, 0);
     }
     if (!hasHeldItem(store)) {
       let cursor = getCursor(store);
@@ -62,13 +59,11 @@ function Content() {
       item.imgSrc = '/images/potion.png';
       setHeldItem(cursor, store, item, 0, 0);
     }
-    createSocketBoxInStore(store, rand(), rand());
     createListBoxInStore(store, rand(5, 3), 3, rand(), rand());
     createFoundryBoxInStore(store, rand(), rand());
     createConnectorBoxInStore(store, rand(), rand());
-    createCraftingBoxInStore(store, rand(), rand());
+    // createCraftingBoxInStore(store, rand(), rand());
   }, []);
-  useViewOrganizer();
 
   return (
     <>
@@ -78,7 +73,7 @@ function Content() {
   );
 }
 
-const MAX_RANGE = 20;
+const MAX_RANGE = 10;
 function rand(max = MAX_RANGE, min = 0) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
